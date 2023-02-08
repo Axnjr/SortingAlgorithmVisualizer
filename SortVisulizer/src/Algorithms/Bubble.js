ef0fbe79dbb2621095f95717b92547e6d52a02a8
Yakshit Chhipa <111436589+Axnjr@users.noreply.github.com>
2023-02-08 06:55:11 +0530

    Add files via upload

---
+++ SortVisulizer/src/Algorithms/Bubble.js
@@ -0,0 +1,40 @@
+import { ChangeColor , ExecuteExitAnimation } from "../AnimationHelpers"
+/**
+ * 
+ * @param {*} arrLen = length of array given by the user .
+ * @param {*} speed = speed of execution for setTimout's and setInterval's .
+ * @param {*} tempArray = temporary array to store the real array ( numArray ) bcuz array state has to be reaplaced not modified / updated .
+ * @param {*} numArray = the real array state .
+ * @param {*} setNumArray = callback to set the new value of numArray
+ * @param {*} setAnimationInProgressBoolean = callback to set / toggle the AnimationInProgressBoolean which is used to disable button's while animating .           
+ * 
+ */
+export const Bubble = (arrLen,speed,tempArray,numArray,setNumArray,setAnimationInProgressBoolean) => {
+
+    let clicks = 0
+
+    ChangeColor(0,"lime",false) 
+
+    let idb = setInterval( () => {
+
+        ChangeColor(clicks,"lime",true)
+
+        for(let i = 0 ; i <= clicks ; i++ ){
+            for(let j = 0 ; j < arrLen ; j++ ){  
+                if(tempArray[i] < tempArray[j]){ 
+                    [tempArray[i],tempArray[j]] = [tempArray[j],tempArray[i]]
+            }}
+            setNumArray([...numArray,tempArray])
+        }
+
+        clicks++
+
+        if( clicks >= arrLen ){ 
+            clearInterval(idb)
+            setAnimationInProgressBoolean(x => !x)
+            setTimeout(() => { ExecuteExitAnimation(true,"yellow",10,arrLen,arrLen) },200)
+        }
+
+    },speed)
+    
+}
